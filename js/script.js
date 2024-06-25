cfce0dc94a95858f56517eeddfb899c3d9325bf3
Arif Momin <arifmomin2743@gmail.com>
2024-06-25 23:59:07 +0600

    last push

--- js/script.js
+++ js/script.js
@@ -30,8 +30,20 @@ window.addEventListener('scroll', function () {
     menu_bar.classList.remove('menu_fixed')
   }
 });
-
-
+// ==============================smooth scrolling
 $(document).ready(function(){
   $('.smooth-scroll').smoothScroll();
 });
+// ============================ifram
+document.querySelector('iframe').addEventListener('click', function() {
+  if (this.style.width === '100%') {
+      this.style.width = '200%';
+      this.style.height = '200%';
+  } else {
+      this.style.width = '100%';
+      this.style.height = '100%';
+  }
+});
+
+
+
